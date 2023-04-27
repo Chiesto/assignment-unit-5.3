20fcf15d9966f2b93fe71c41fab824ddf7454bce
Chris Clark <xchrisck@gmail.com>
2023-04-27 10:41:39 -0500

    created a function that adds music to my collection array. made an object 'music' within that function to use the input parameters title, artist, and year published.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,17 @@
 console.log('***** Music Collection *****')
+let collection = [];
+function addToCollection(title, artist, yearPublished){
+    const music = {
+        title,
+        artist,
+        yearPublished
+    };
+    collection.push(music);
+    return music;
+}
+//end addToCollection - adds music to my music collection 
+//(title, artist, year published).
+addToCollection('Pursuit of Happiness', 'Kid Cudi', '2009');
+console.log(collection);
+//execute the function addToCollection and test it by 
+//console logging the newly updated collection.
